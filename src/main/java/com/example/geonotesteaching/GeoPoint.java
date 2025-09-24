02d36e69a48f2f4bd056500efc86385c226951ca
Juanfu224 <juanfu224@gmai.com>
2025-09-24 22:44:20 +0200

    Mejoras legibilidad

--- src/main/java/com/example/geonotesteaching/GeoPoint.java
+++ src/main/java/com/example/geonotesteaching/GeoPoint.java
@@ -4,10 +4,13 @@ package com.example.geonotesteaching;
 // el constructor, los getters, los métodos equals(), hashCode() y toString().
 // A diferencia de Kotlin, los tipos de los campos deben ser explícitos.
 public record GeoPoint(double lat, double lon) {
-    // El 'constructor compacto' de un record se usa para validar los datos de los campos.
+    // El 'constructor compacto' de un record se usa para validar los datos de los
+    // campos.
     // Es una buena práctica para asegurar la consistencia del objeto.
     public GeoPoint {
-        if (lat < -90 || lat > 90) throw new IllegalArgumentException("Latitud inválida: " + lat);
-        if (lon < -180 || lon > 180) throw new IllegalArgumentException("Longitud inválida: " + lon);
+        if (lat < -90 || lat > 90)
+            throw new IllegalArgumentException("Latitud inválida: " + lat);
+        if (lon < -180 || lon > 180)
+            throw new IllegalArgumentException("Longitud inválida: " + lon);
     }
 }
